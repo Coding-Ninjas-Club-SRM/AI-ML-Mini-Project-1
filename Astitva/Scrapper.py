6af910ff80a17e6f89d607a2f390674f8d0b7536
astitvaveergarg <91984568+astitvaveergarg@users.noreply.github.com>
2021-10-22 13:15:12 +0530

    V1.0

---
+++ Astitva/Scrapper.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
+import csv
+driver = webdriver.Chrome('Astitva\chromedriver_win32\chromedriver.exe')
+driver.get("https://www.sbipensionfunds.com/historical-nav/")
+datefield1=driver.find_element_by_id('f_date_p1')
+datefield2=driver.find_element_by_id('f_date_p2')
+ActionChains(driver).move_to_element(datefield1).click().send_keys('15-05-2009').perform()
+ActionChains(driver).move_to_element(datefield2).click().send_keys('17-10-2021').perform()
+search_btn = driver.find_element_by_name('mysubmit')
+ActionChains(driver).move_to_element(search_btn).click().click().perform()
+
+
+filename='test.csv'
+csv_writer=csv.writer(open(filename,'w'))
+
+
+soup = BeautifulSoup(driver.page_source,'html.parser')
+
+
+for tr in soup.find_all('tr')[1:]:
+    data=[]
+
+    for td in tr.find_all('td'):
+        data.append(td.text.rstrip())
+    if(data):
+        print("Inserting Table data: {}".format(','.join(data)))
+        csv_writer.writerow(data)
+        
+
+
+
+
+
