0ee92692dbc37f4bed940f1d88cb0fa6e6e16658
astitvaveergarg <av4764@srmist.edu.in>
2021-10-24 10:40:35 +0530

    V 1.1

--- Astitva/Scrapper.py
+++ Astitva/Scrapper.py
@@ -8,7 +8,7 @@ driver.get("https://www.sbipensionfunds.com/historical-nav/")
 datefield1=driver.find_element_by_id('f_date_p1')
 datefield2=driver.find_element_by_id('f_date_p2')
 ActionChains(driver).move_to_element(datefield1).click().send_keys('15-05-2009').perform()
-ActionChains(driver).move_to_element(datefield2).click().send_keys(date.today().strftime("%d-%m-%Y")).perform()
+ActionChains(driver).move_to_element(datefield2).click().send_keys(date.today().strftime("%d-%m-%y")).perform()
 search_btn = driver.find_element_by_name('mysubmit')
 ActionChains(driver).move_to_element(search_btn).click().click().perform()
 
